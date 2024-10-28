fc8b53bf0372df51593e4bf74e936f337e4578e5
Derek Mikesell <derekjm@amazon.com>
2024-10-28 18:00:13 +0000

    Update initial files

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # GluonTSModels
-Personal GluonTS model exploration.
+
+GluonTSModels is a Python package that provides implementations and utilities for time series forecasting models using GluonTS.
+
+## Installation
+
+You can install the package using pip:
+
+```bash
+cd GluonTSModels
+pip install .
+```
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gluontsmodels",
+    version="0.1.0",
+    packages=find_packages(exclude=["tests*"]),
+    install_requires=[
+        "gluonts=0.13.2",
+        "torch=1.13"
+        # Add your dependencies here, for example:
+        # "gluonts>=0.6.0",
+        # "numpy>=1.18.0",
+        # "pandas>=1.0.0",
+    ],
+    author="Derek Mikesell",
+    author_email="derekmikesell@gmail.com",
+    description="A package for GluonTS models",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/GluonTSModels",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
